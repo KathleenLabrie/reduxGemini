bed40b004fdd12454ee6bf99fa23a106faa66ad0
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2014-10-06 10:27:55 -0600

    Added a bunch of doc placeholders and created setup.py

---
+++ reduxGemini/install_support_files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from pkg_resources import Requirement, resource_filename
+import shutil
+import os
+import sys
+import argparse
+
+SHORT_DESCRIPTION = 'Install the documentation'
+
+def parse_args(command_line_args):
+    parser = argparse.ArgumentParser(prog='getdocs',
+                                     description=SHORT_DESCRIPTION)
+    parser.add_argument('destination', action='store', nargs=1,
+                        type=str, default=None,
+                        help='Where to put the docs')
+    
+    args = parser.parse_args(command_line_args)
+    
+    return args
+
+def build_docs():
+    pass
+
+def move_docs(destination):
+    
+
+    dirname = resource_filename(Requirement.parse('reduxGemini'),
+                            os.path.join('share', 'reduxGemini'))
+    #if not os.path.isdir(dirname):
+    #    dirname = resources_filename(Requirement.parse('reduxGemini'), 'doc')
+    #    sub_destination = os.path.join(destination, 'reduxGemini', 'doc')
+    #else:
+    #    sub_destination = os.path.join(destination, 'reduxGemini')
+    
+    sub_destination = os.path.join(destination, 'reduxGemini')
+
+    if os.path.isdir(os.path.join(sub_destination)):
+        shutil.rmtree(os.path.join(sub_destination))
+    shutil.copytree(dirname, sub_destination)
+        
+    return
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    args = parse_args(argv)
+    
+    move_docs(args.destination[0])
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long_description from the Description.rst file
+with open(os.path.join(cwd, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+MODULENAME = 'reduxGemini'
+
+DATA_FILES = []
+DOC_FILES = [(os.path.join('share',MODULENAME,root), [os.path.join(root,f) for f in files]) \
+              for root, dirs, files in os.walk('doc')]
+DATA_FILES.extend(DOC_FILES)
+print '***********************'
+print 'data_files = ', DOC_FILES
+print '***********************'
+
+
+setup(
+      name = 'reduxGemini',
+      version = '0.1.0dev1',
+      
+      description = 'Tools and documentation to reduce Gemini data',
+      long_description = long_description,
+      
+      url = 'https://github.com/KathleenLabrie/reduxGemini',
+      
+      author = 'Kathleen Labrie',
+      author_email = 'kathleen.labrie.phd@gmail.com',
+      
+      #licence = '',
+      
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers = [
+                    'Development Status :: 2 - Pre-Alpha',
+                    'Intended Audience :: Science/Research',
+                    'License ::',
+                    'Operating System :: Mac OS :: MacOS X',
+                    'Operating System :: POSIX :: Linux',
+                    'Programming Language :: Python :: 2.7',
+                    'Topic :: Scientific/Engineering :: Astronomy'
+                    ],
+      
+      keywords = 'Gemini data reduction',
+      
+      packages = find_packages(exclude=['doc']),
+      
+      #install_requires = ['']
+      #extras_require = {
+      #  'dev': [''],
+      #},
+      
+      #package_data = {
+      #                 'reduxGemini': [''],
+      #                },
+      
+      data_files = DATA_FILES,
+      
+      scripts = [
+                 'reduxGemini/gmos/scripts/fgmosLS'
+                 ],
+
+      )
